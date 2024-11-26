e545f38108713bbf1fb631716cdad410f8e4700a
aubrey whitford <aubreya.whitford@gmail.com>
2024-11-25 20:09:03 -0500

    Add like button, swatch list to displat color shades, div to display current color info, div for each core color swatch,  main swatch div to display current selected color

--- index.js
+++ index.js
@@ -16,8 +16,22 @@ document.addEventListener("DOMContentLoaded", () => {
         const previewSection = document.getElementById("preview-section");
         previewSection.classList.add("#preview-section");
         // add style
-      
-
+        const mainSwatch = document.createElement("div");
+        mainSwatch.classList.add("#div");
+        // add style
+        const likeButton = document.createElement("button"); 
+        likeButton.classList.add("#button");
+        // add style
+        const swatchList = document.createElement("ul");
+        swatchList.classList.add("#ul");
+        // add style
+        const colorInfoDiv = document.createElement("div");
+        colorInfoDiv.classList.add("#div");
+        // add style
+        const colorDiv = document.createElement("div");
+        colorDiv.classList.add("#div");
+        // add style
     })
+    
 })
     
